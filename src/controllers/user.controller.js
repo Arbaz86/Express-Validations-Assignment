22bdf7f6df42040676d01f2a006f38d204214836
Arbaz86 <arbaz10042002@gmail.com>
2022-03-25 11:38:43 +0530

    completed the Express Validations assignment

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,15 +6,32 @@ const User = require("../models/user.model");
 
 router.post(
   "/",
-  body("first_name").not().isEmpty(),
-  body("last_name").not().isEmpty(),
-  body("email").isEmail(),
-  body("pincode").isLength({ min: 6, max: 6 }),
-  body("age").not().isEmpty().isNumeric().custom((value) => {
-    if(value < 1 || value > 100){
-      
-    }
-  }),
+  body("first_name").not().isEmpty().withMessage("first_name not be empty"),
+  body("last_name").not().isEmpty().withMessage("last_name not be empty"),
+  body("email")
+    .isEmail()
+    .custom(async (value) => {
+      const user = await User.findOne({ email: value });
+
+      if (user) {
+        throw new Error("email is already taken");
+      }
+      return true;
+    }),
+  body("pincode")
+    .isLength({ min: 6, max: 6 })
+    .withMessage("Picode must be 6 characters "),
+  body("age")
+    .not()
+    .isEmpty()
+    .withMessage("age cannot be Empty")
+    .isNumeric()
+    .custom((value) => {
+      if (value < 1 || value > 100) {
+        throw new Error(" age must be between 1 to 100 ");
+      }
+      return true;
+    }),
   async (req, res) => {
     try {
       const errors = validationResult(req);
